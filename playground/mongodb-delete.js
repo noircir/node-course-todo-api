cccac5a8b54995180a7e5173891c343514fe7e4f
Nellie K <noircir@users.noreply.github.com>
2018-07-26 20:40:17 -0400

    Add delete script

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -5,26 +5,50 @@ const {MongoClient, ObjectID} = require('mongodb');
 // console.log(obj)
 
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }, (err, client) => {
+MongoClient.connect('mongodb://localhost:27017/ToDoApp', { useNewUrlParser: true }, (err, client) => {
 	if(err) {
 		return console.log('Unable to connect to MongoDB server');
 	}
 	console.log('Connected to MongoDB server');
 
 	// 'client' is needed because of MongoDB does it now this way
-	const db = client.db('TodoApp');
+	const db = client.db('ToDoApp');
 
-	db.collection('Todos').find({
+	// deleteMany
 
-		// _id is now an objectID deconstructed above (line 2)
-		_id: new ObjectID('5b5545c1307ea8d750d60dd1')
-		// toArray() returns Promise; it is async, do some action with the result
-	}).toArray().then((docs) => {
-		console.log('Todos');
-		console.log(JSON.stringify(docs, undefined, 2));
+	// db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((res) => {
+	// 	console.log('Deleted Todos: ');
+	// 	console.log(res);
+	// }, (err) => {
+	// 	console.log('Unable to fetch todos', err)
+	// });
+
+	// db.collection('Users').deleteMany({name: 'Bob Marley'}).then((res) => {
+	// 	console.log('Deleted Users: ');
+	// 	console.log(res);
+	// }, (err) => {
+	// 	console.log('Unable to delete todos', err)
+	// });
+
+	// deleteOne 
+
+	// db.collection('Todos').deleteOne({text: 'lunch'}).then((res) => {
+	// 	console.log('Deleted Todos: ');
+	// 	console.log(JSON.stringify(res, undefined, 2));
+	// }, (err) => {
+	// 	console.log('Unable to delete todos', err)
+	// });
+
+	// findOneAndDelete
+
+	db.collection('Users').findOneAndDelete({_id: ObjectID('5b55300cc90b890dacbe86da')}).then((res) => {
+		console.log('Deleted Users: ');
+		// console.log(JSON.stringify(res, undefined, 2));
+		console.log(res);
 	}, (err) => {
-		console.log('Unable to fetch todos', err)
+		console.log('Unable to delete users', err)
 	});
+
 
 	client.close();
 });
