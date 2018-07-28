3947f5788434c9cba2c62575bd5fb17b4c864eeb
Nellie K <noircir@users.noreply.github.com>
2018-07-28 15:55:06 -0400

    Refactoring: add /db and /models

---
+++ server/db/mongoose.js
@@ -0,0 +1,7 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+
+module.exports = {
+	mongoose: mongoose
+}
--- server/server.js
+++ server/server.js
@@ -1,67 +1,16 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-//mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+// ES6 destructuring
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-var Todo = mongoose.model('Todo', {
-	text: {
-		type: String,
-		required: true,
-		minlength: 1,
-		trim: true
-	},
-	completed: {
-		type: Boolean,
-		default: false
-	},
-	completedAt: {
-		type: Number,
-		default: null
-	}
+var app = express();
+
+
+
+
+app.listen(3000, () => {
+	console.log('Started on port 3000...');
 });
-
-// var newTodo = new  Todo({
-// 	text: 'Cook dinner'
-// });
-
-// newTodo.save().then(() => {
-// 	console.log('Saved todo', newTodo);
-// }, (e) => {
-// 	console.log('Unable to save todo');
-// })
-
-// var newTodo2 = new Todo({
-// 	text: 'Ride a bike',
-// 	completed: false,
-// 	completedAt: 8
-// });
-
-// var newTodo2 = new Todo({
-// 	text: "Play tennis    "
-// });
-
-// newTodo2.save().then(() => {
-// 	console.log('Saved todo: ', newTodo2);
-// }, (e) => {
-// 	console.log(e);
-// });
-
-// Challenge
- var User = mongoose.model ('User', {
- 	email: {
- 		type: String,
-	 	required: true,
-	 	trim: true, 
-	 	minlength: 1
- 	}
- });
-
- var Kevin3 = new User({
- 	email: "Kevin448@gmail.com"
- })
-
- Kevin3.save().then(() => {
- 	console.log(Kevin3);
- }, (e) => {
- 	console.log(e);
- });
