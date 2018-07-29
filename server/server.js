b95a7201f583171e3be3c8d034728c739b74806c
Nellie K <noircir@users.noreply.github.com>
2018-07-28 20:29:06 -0400

    Add POST /todos route

--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-// ES6 destructuring
+// ES6 destructuring. Creating a local variable 'mongoose'
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
 
+// bodyParser.json() returns a function that we need to give to express.
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+	var todo = new Todo({
+		text: req.body.text
+	});
+
+	todo.save().then((doc) => {
+		res.send(doc);
+	}, (e) => {
+		res.status(400).send(e);
+	});
+
+});
 
 
 
