7ac804f36805c446fe869865d69679c463174014
Nellie K <noircir@users.noreply.github.com>
2018-07-31 18:52:26 -0400

    Tweak mlab connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,9 +5,10 @@ var mongoose = require('mongoose');
 
 let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://nellie:mogorov12@ds159651.mlab.com:59651/nodeplayground1'
+  mlab: 'mongodb://monty:monty123@ds159651.mlab.com:59651/nodeplayground1'
 };
-mongoose.connect( db.localhost, { useNewUrlParser: true } || db.mlab, { useNewUrlParser: true });
+// mongoose.connect( db.localhost, { useNewUrlParser: true } || db.mlab, { useNewUrlParser: true });
+mongoose.connect( db.localhost|| db.mlab);
 
 module.exports = {
 	mongoose: mongoose
