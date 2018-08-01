aef1fad02ae2c9808cfc27f21312cfa11283f6af
Nellie K <noircir@users.noreply.github.com>
2018-07-31 20:22:58 -0400

    Tweak mlab connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,7 +6,7 @@ mongoose.connect('mongodb://monty:monty123@ds159651.mlab.com:59651/nodeplaygroun
 // let db = {
 //   localhost: 'mongodb://localhost:27017/TodoApp',
 //   mlab: 'mongodb://monty:monty123@ds159651.mlab.com:59651/nodeplayground1'
-};
+// };
 // mongoose.connect( db.localhost, { useNewUrlParser: true } || db.mlab, { useNewUrlParser: true });
 mongoose.connect( db.localhost|| db.mlab);
 
