29f18056229fb85fe2ea8a74b1db2c1c0a5d47dd
Nellie K <noircir@users.noreply.github.com>
2018-08-02 17:52:47 -0400

    Setup mongoose.js for heroku with MONGODB_URI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,11 +1,13 @@
 var mongoose = require('mongoose');
 
-let db = {
-  localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://monty:monty123@ds159651.mlab.com:59651/nodeplayground1'
-};
+// let db = {
+//   localhost: 'mongodb://localhost:27017/TodoApp',
+//   mlab: 'mongodb://monty:monty123@ds159651.mlab.com:59651/nodeplayground1'
+// };
 
-mongoose.connect( db.localhost || db.mlab, { useNewUrlParser: true });
+// mongoose.connect( db.localhost || db.mlab, { useNewUrlParser: true });
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true } );
 
 module.exports = {
 	mongoose: mongoose
