263d960db29f61aad3b950eb8f2d7402fa6bb458
Nellie K <noircir@users.noreply.github.com>
2018-08-02 08:45:01 -0400

    Tweak mongoose connection to localhost || heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,11 +1,15 @@
 var mongoose = require('mongoose');
 
+// var mongodb_uri = 'mongodb://monty:monty123@ds159651.mlab.com:59651/nodeplayground1';
+// mongoose.connect(mongodb_uri, { useNewUrlParser: true });
+
+
 let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
   mlab: 'mongodb://monty:monty123@ds159651.mlab.com:59651/nodeplayground1'
 };
-mongoose.connect( db.localhost, { useNewUrlParser: true } || db.mlab, { useNewUrlParser: true });
-// mongoose.connect( db.localhost|| db.mlab);
+// mongoose.connect( db.localhost, { useNewUrlParser: true } || db.mlab, { useNewUrlParser: true });
+mongoose.connect( db.mlab || db.localhost, { useNewUrlParser: true });
 
 module.exports = {
 	mongoose: mongoose
