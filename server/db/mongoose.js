dc6db8714d3b2b9746f4da1d58e329c4b19e0943
Nellie K <noircir@users.noreply.github.com>
2018-08-02 08:03:23 -0400

    Edit mlab connection in mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,13 +1,10 @@
 var mongoose = require('mongoose');
 
-// mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
-mongoose.connect('mongodb://monty:monty123@ds159651.mlab.com:59651/nodeplayground1', { useNewUrlParser: true });
-
-// let db = {
-//   localhost: 'mongodb://localhost:27017/TodoApp',
-//   mlab: 'mongodb://monty:monty123@ds159651.mlab.com:59651/nodeplayground1'
-// };
-// mongoose.connect( db.localhost, { useNewUrlParser: true } || db.mlab, { useNewUrlParser: true });
+let db = {
+  localhost: 'mongodb://localhost:27017/TodoApp',
+  mlab: 'mongodb://monty:monty123@ds159651.mlab.com:59651/nodeplayground1'
+};
+mongoose.connect( db.localhost, { useNewUrlParser: true } || db.mlab, { useNewUrlParser: true });
 // mongoose.connect( db.localhost|| db.mlab);
 
 module.exports = {
