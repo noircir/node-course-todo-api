bd3f390fe51299f97afd9fb075ee3581f5b7675d
Nellie K <noircir@users.noreply.github.com>
2018-08-12 21:31:25 -0400

    Expand user model

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,10 @@
 var env = process.env.NODE_ENV || 'development';
+
+// on Heroku, process.env.NODE_ENV = 'production'
+
 console.log('env ******', env);
 
-if (env == 'development') {
+if (env === 'development') {
 	process.env.PORT = 3000;
 	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
 } else if (env === 'test') {
--- server/server.js
+++ server/server.js
@@ -112,6 +112,21 @@ app.patch('/todos/:id', (req, res) => {
 });
 
 
+app.post('/users', (req, res) => {
+
+	var body = _.pick(req.body, ['email', 'password']);
+
+	var user = new User(body);
+	
+	user.save().then((doc) => {
+		res.send(doc);
+	}, (e) => {
+		res.status(400).send(e);
+	});
+});
+
+
+
 
 app.listen(port, () => {
 	console.log(`Started up at port ${port}...`);
